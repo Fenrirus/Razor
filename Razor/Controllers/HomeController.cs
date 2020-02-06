bece283d0dbd0a40b8d4c425156642205c99afa6
Marian Marian <fenrir55@gmail.com>
2020-02-06 21:21:04 +0100

    Start

--- Razor/Controllers/HomeController.cs
+++ Razor/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Razor.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +9,18 @@ namespace Razor.Controllers
 {
     public class HomeController : Controller
     {
+        private Product myProduct = new Product()
+        {
+            ProductId = 1,
+            Name = "Kajak",
+            Description = "Jednoosobowa łodka",
+            Category = "Sporty Wodne",
+            Price = 275M,
+        };
+
         public ActionResult Index()
         {
-            return View();
+            return View(myProduct);
         }
 
         public ActionResult About()
